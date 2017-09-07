270d9974242f06e3cbcdd8f79d832d959898fb30
lorence <lorence@enclave.vn>
2017-09-07 09:20:53 +0700

    Fix camera fragm

---
+++ app/src/main/java/com/example/lorence/camerautils/CameraUtils.java
@@ -0,0 +1,13 @@
+package com.example.lorence.camerautils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraUtils extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_utils);
+    }
+}
